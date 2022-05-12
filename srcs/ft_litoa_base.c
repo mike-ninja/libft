7d3655178ccd481c87fc8099861e18d7a016ad47
MrMikey695 <mikeedubas@hotmail.com>
2022-05-12 14:19:46 +0300

    update

---
+++ srcs/ft_litoa_base.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_litoa.base.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbarutel <mbarutel@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/12 13:47:32 by mbarutel          #+#    #+#             */
+/*   Updated: 2022/05/12 14:10:23 by mbarutel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../incs/libft.h"
+
+/*
+**	ft_char_val:
+**	: Fetches the character value from the int
+*/
+static char	ft_char_val(unsigned long int val)
+{
+	char	ret;
+
+	if (val >= 10)
+		ret = (val - 10) + 'a';
+	else
+		ret = val + '0';
+	return (ret);
+}
+
+/*
+**	ft_deci_len:
+**	: Calculates the amount of digits based on the base. Kind of like ft_strlen
+*/
+static int	ft_deci_len(unsigned long int val, int base)
+{
+	int	len;
+
+	len = 0;
+	while (val != 0)
+	{
+		len++;
+		val /= base;
+	}
+	return (len);
+}
+
+/*
+**	ft_itoa_base:
+**	: Takes in a value and converts it to string based on base.
+*/
+char	*ft_litoa_base(long int value, int base)
+{
+	int					len;
+	unsigned long int	tmp;
+	char				*ret;
+
+	len = 0;
+	if (value < 0 && base == 10)
+	{
+		tmp = value * -1;
+		len++;
+	}
+	else
+		tmp = (unsigned long int)value;
+	len += ft_deci_len(tmp, base);
+	ret = (char *)malloc(len + 1);
+	if (!ret)
+		return (NULL);
+	ret[len] = '\0';
+	while (tmp)
+	{
+		ret[--len] = ft_char_val(tmp % base);
+		tmp /= base;
+	}
+	if (value < 0 && base == 10)
+		ret[--len] = '-';
+	return (ret);
+}
